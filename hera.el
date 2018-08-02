61c5ca0e34cdec61c86041a5baa7dcc263e4b0f8
Dustin Lacewell <dlacewell@gmail.com>
2018-08-02 06:09:28 -0500

    Remove major-mode features and fixup implementation

--- hera.el
+++ hera.el
@@ -1,56 +1,31 @@
-;;; hera.el --- Hera for Gydra
+;;; hera.el --- Hera for Hydra
 ;; Copyright (C) 2018 Dustin Lacewell
 
 ;; Author: Dustin Lacewell <dlacewell@gmail.com>
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24") (hydra "0") (ht "0") (dash "0"))
+;; Package-Requires: ((emacs "24") (hydra "0"))
 ;; Keywords: hydra
 ;; URL: http://github.com/dustinlacewell/hera
 
 ;;; Commentary:
 
-;; This package is awesome!
+;; This package provides a simple stack feature for Hydra.
 
 ;;; Code:
 (require 'hydra)
-(require 'ht)
-(require 'dash)
-
-;; the hydra body to run if no major mode specific hydra is set
-(defvar hera-default-hydra nil)
-
-;; this is an alist of major mode -> hydra body
-(defvar hera--hydras (ht))
 
 ;; this is a FIFO list of hydra bodies
-(defvar hera--stack nil)
-
-;; return the hydra body for the current major-mode, or hera-entrypoint
-(defun hera--first-body ()
-  (let ((current-hydra (ht-get hera--hydras major-mode)))
-    (if current-hydra (call-interactively current-hydra)
-      hera-default-hydra)))
-
-(defun hera-register (major-mode hydra-body)
-  (ht-set! hera--hydras major-mode hydra-body))
+(setq hera--stack nil)
 
 (defun hera-push (hydra-body)
-  (setq hera--stack (-cons* hydra-body hera--stack))
+  (when hydra-curr-body-fn
+    (push hydra-curr-body-fn hera--stack))
   (call-interactively hydra-body))
 
 (defun hera-pop ()
-  (if (length hera--stack)
-      (cl-destructuring-bind (hydra-body rest) (-split-at 1 hera--stack)
-        (setq hera--stack rest)
-        (when hydra-body
-          (call-interactively (car hydra-body))))
-    (hydra--body-exit)))
-
-;; execute the hydra body for the current mode, or the default one
-(defun hera-run ()
-  (interactive)
-  (let ((context (hera--first-body)))
-    (when context (funcall context))))
+  (let ((x (pop hera--stack)))
+    (when x
+      (call-interactively x))))
 
 (provide 'hera)
 ;;; hera.el ends here
